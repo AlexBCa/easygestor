269528c7b4f8fa5ebf2bc64b672d2b3d31af8ee8
AlexBCa <alexbeca92@gmail.com>
2021-04-14 19:47:06 +0200

    Primer commit

---
+++ easygestor/src/main/java/es/alex/easygestor/SecondaryController.java
@@ -0,0 +1,12 @@
+package es.alex.easygestor;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
