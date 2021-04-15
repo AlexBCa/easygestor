0f6ae9a40ac09a616e961e2d49d4ba9145ab59be
AlexBCa <alexbeca92@gmail.com>
2021-04-15 18:43:51 +0200

    Se añadido vista principal, gestion usuario, gestion libro.

--- easygestor/src/main/java/es/alex/easygestor/PrimaryController.java
+++ easygestor/src/main/java/es/alex/easygestor/PrimaryController.java
@@ -1,12 +1,48 @@
 package es.alex.easygestor;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+public class PrimaryController implements Initializable {
+	
+    @FXML
+    private BorderPane panelPrincipal;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    
+    @FXML
+    void panelUsuario(MouseEvent event) {
+    	
+
+    }
+    
+    private void loadUI(String ui) {
+    	Parent root = null;
+    	try {
+			root = FXMLLoader.load(getClass().getResource(ui+ ".fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	panelPrincipal.setCenter(root);
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+		loadUI("ui_usuarios");
+		
+	}
 }
