31396cdedd6ce19b58be138ae63832b815da4dc1
AlexBCa <alexbeca92@gmail.com>
2021-04-17 12:02:42 +0200

    Añadir clase Libro

---
+++ easygestor/src/main/java/model/Libro.java
@@ -0,0 +1,102 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Libro")
+public class Libro implements Serializable {
+	
+	
+	private int id_libro;
+	@Id
+	@Column(name="isbn")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int isbn;
+	private String titulo;
+	private String autores;
+	private String editorial;
+	private String edicion;
+	private Boolean disponibilidad;
+	
+	public Libro() {
+		
+	}
+
+	public Libro(int id_libro, int isbn, String titulo, String autores, String editorial, String edicion,
+			Boolean disponibilidad) {
+		super();
+		this.id_libro = id_libro;
+		this.isbn = isbn;
+		this.titulo = titulo;
+		this.autores = autores;
+		this.editorial = editorial;
+		this.edicion = edicion;
+		this.disponibilidad = disponibilidad;
+	}
+
+	public int getId_libro() {
+		return id_libro;
+	}
+
+	public void setId_libro(int id_libro) {
+		this.id_libro = id_libro;
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutores() {
+		return autores;
+	}
+
+	public void setAutores(String autores) {
+		this.autores = autores;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+
+	public String getEdicion() {
+		return edicion;
+	}
+
+	public void setEdicion(String edicion) {
+		this.edicion = edicion;
+	}
+
+	public Boolean getDisponibilidad() {
+		return disponibilidad;
+	}
+
+	public void setDisponibilidad(Boolean disponibilidad) {
+		this.disponibilidad = disponibilidad;
+	}
+	
+	
+
+}
