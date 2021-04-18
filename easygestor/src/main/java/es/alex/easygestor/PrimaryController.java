ad19d33c3c8a8a85fe628172e0e3fb062991f3cc
AlexBCa <alexbeca92@gmail.com>
2021-04-18 20:02:34 +0200

    Añadidas funciones para cambiar de ventanas.

--- easygestor/src/main/java/es/alex/easygestor/PrimaryController.java
+++ easygestor/src/main/java/es/alex/easygestor/PrimaryController.java
@@ -2,6 +2,9 @@ package es.alex.easygestor;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -10,6 +13,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import model.Crud;
+import model.Libro;
+import model.Prestamo;
+import model.Usuario;
 
 /**
  * Esta clase controla los eventos lanzados al pulsar botones en la interfaz.
@@ -22,16 +29,34 @@ public class PrimaryController implements Initializable {
     @FXML
     private BorderPane panelPrincipal;
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
     
     @FXML
     void panelUsuario(MouseEvent event) {
     	
+    	loadUI("ui_usuarios");
 
     }
+    
+    @FXML
+    void panelBiblioteca(MouseEvent event) {
+    	loadUI("ui_libros");
+
+    }
+
+    @FXML
+    void panelConfig(MouseEvent event) {
+    	loadUI("ui_config");
+
+    }
+
+    @FXML
+    void panelPrestamo(MouseEvent event) {
+    	loadUI("ui_prestamo");
+
+    }
+
+
+
     /**
      * Metodo para cargar las distintas pantallas.
      * @param ui. Este parametro permitira escoger las distintas interfaces.
@@ -55,6 +80,65 @@ public class PrimaryController implements Initializable {
 		// TODO Auto-generated method stub
 		
 		loadUI("ui_usuarios");
+		
+		System.out.println("Hola");
+		
+		Usuario user = new Usuario();
+		user.setNsocio(2541615);
+		user.setDni("55d56d");
+		user.setNombre("Ana");
+		user.setApellidos("Jimenez");
+		user.setDireccion("calle falsa 123");
+		user.setEmail("email@falso.com");
+		user.setTelefono(955555);
+		
+		Usuario useralt = new Usuario();
+		useralt.setNsocio(2541615);
+		useralt.setDni("55d56d");
+		useralt.setNombre("Ana");
+		useralt.setApellidos("Jimenez Rodriguez");
+		useralt.setDireccion("calle falsa 123");
+		useralt.setEmail("email@falso.com");
+		useralt.setTelefono(955555);
+		
+		Libro l = new Libro();
+		l.setIsbn(585855);
+		l.setAutores("Dawking");
+		l.setEdicion("primera");
+		l.setEditorial("ni idea");
+		l.setTitulo("El gen egoista");
+		l.setDisponibilidad(true);
+		
+		Prestamo pr = new Prestamo();
+		pr.setIsbn(585855);
+		pr.setNsocio(2541615);
+		
+		long now = System.currentTimeMillis();
+        Date sqlDate = new Date(now);
+        
+        // Sumamos 15 dias en ms
+        Date limite = new Date(now+1296000000);
+        
+		pr.setFecha_prestamo(sqlDate);
+		pr.setFecha_limite_prestamo(limite);
+		
+		
+		Crud c = new Crud();
+		
+		//c.setup();
+		/*
+		 
+		 
+		ArrayList<Usuario> usuarios = (ArrayList<Usuario>) c.findAllUsuarios();
+		
+		for(Usuario u: usuarios) {
+			System.out.println(u.getNombre());
+		}
+		*/
+		
+		
+		
+		
 		
 	}
 }
