352479929dd7c65398ae5fb363d8dd60d778c413
AlexBCa <alexbeca92@gmail.com>
2021-04-23 12:27:11 +0200

    add cargarTabla, obtenerFoco, botonAdd, botonEdit, cargatablaper, del

--- easygestor/src/main/java/es/alex/easygestor/UsuarioController.java
+++ easygestor/src/main/java/es/alex/easygestor/UsuarioController.java
@@ -1,23 +1,35 @@
 package es.alex.easygestor;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import model.Crud;
 import model.Usuario;
 
-public class UsuarioController implements Initializable {
+public class UsuarioController extends PanelPadre implements Initializable {
 	
 
     @FXML
@@ -58,15 +70,43 @@ public class UsuarioController implements Initializable {
 		manager = new Crud();
 		manager.setup();
 		
-		cargarUsuariosTabla();
+		
+		cargarTabla();
+		botonEdit(resources);
+		cambios();
+		buscar_user.setPromptText("Buscar Apellidos");
+		
+		add_user.setOnAction(this::botonAdd);
+		del_user.setOnAction(this::botonDel);
+		
+		
+		
+		
+		
+		
+		tabla_user.getSelectionModel().selectedItemProperty().addListener(new javafx.beans.value.ChangeListener<Usuario>() {
+			@Override
+			public void changed(ObservableValue<? extends Usuario> observable, Usuario oldValue, Usuario newValue) {
+				if (tabla_user.getSelectionModel().getSelectedItem() != null) {
+					edit_user.setDisable(false);
+					del_user.setDisable(false);
+				} else {
+					edit_user.setDisable(true);
+					del_user.setDisable(true);
+				}
+			}
+		});
+		
+	
 		
 	}
 	
-	public void cargarUsuariosTabla() {
+	
+	public void cargarTabla() {
 			
 			tabla_user.getItems().clear();
 			List<Usuario> usuarios = manager.findAllUsuarios();
-			//Creamos una lista observable donde se guardaran todos los objetos usuarios.
+			//Creamos un objeto Observable donde se guardarán todos los objetos usuarios.
 			ObservableList<Usuario> listaUsuarios = FXCollections.observableArrayList(usuarios);
 			
 			
@@ -77,9 +117,128 @@ public class UsuarioController implements Initializable {
 			columApellidos.setCellValueFactory(new PropertyValueFactory<Usuario, String>("apellidos"));
 			
 			
-			//Añadimos los servicos a la tabla.
+			//Añadimos los Usuarios a la tabla.
 			tabla_user.setItems(listaUsuarios);
 			
 		}
+	
+	
+	public void alerta(String nombre, Alert.AlertType alert) {
+		Alert alerta = new Alert(AlertType.ERROR, nombre);
+		
+		alerta.showAndWait();
+	}
+	
+	public Object obtenerObjetoFoco() {
+		ObservableList<?> listaUser;
+		
+		listaUser = tabla_user.getSelectionModel().getSelectedItems();
+		
+		if(listaUser.isEmpty()) {
+			alerta("Selecione una fila para borrar", AlertType.ERROR);
+			return 0;
+		}
+		return listaUser.get(0);
+	}
+	
+	public void botonAdd(ActionEvent event) {
+		try {
+			// Cargamos la informacion de la ventana de edicion que abriremos
+			FXMLLoader loader = new FXMLLoader(App.class.getResource("ui_usuario_add.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("Editar registro");
+			Parent root = loader.load();
+			stage.setScene(new Scene(root));
+			stage.show();
+			
+			// Evento que se lanzara al cerrar la ventana de edicion y actualizara los datos de la tabla
+			stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				@Override
+				public void handle(WindowEvent event) {
+					cargarTabla();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	
+		
+	}
+	
+	public void botonEdit(ResourceBundle resources) {
+		edit_user.setOnMouseClicked(event -> {
+			try {
+				// Cargamos la informacion de la ventana de edicion que abriremos
+				FXMLLoader loader = new FXMLLoader(App.class.getResource("ui_usuario_add.fxml"), resources);
+				Stage stage = new Stage();
+				stage.setTitle("Editar registro");
+				Parent root = loader.load();
+				stage.setScene(new Scene(root));
+				// Añadimos la fila seleccionada al controlador para poder editarlo
+				((AddUsuarioController) loader.getController()).initData(tabla_user.getSelectionModel().getSelectedItem());
+				stage.show();
+				// Evento que se lanzara al cerrar la ventana de edicion y actualizara los datos de la tabla
+				stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+					@Override
+					public void handle(WindowEvent event) {
+						cargarTabla();
+					}
+				});
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+	
+	public void botonDel(ActionEvent event) {
+		
+		Usuario user = (Usuario) obtenerObjetoFoco();
+		if(user != null) {
+			manager.delete(user);
+		}
+		
+		cargarTabla();
+		
+		
+	}
+	
+	// Buscar
+	
+	public void cambios() {
+		
+		buscar_user.textProperty().addListener(new ChangeListener<String>() {
+
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				// TODO Auto-generated method stub
+				System.out.println("Cambios");
+				cargarTablaPersonalizada(buscar_user.getText().toString());
+			}
+			
+		});
+		
+	}
+	
+	public void cargarTablaPersonalizada(String apellido) {
+		tabla_user.getItems().clear();
+		List<Usuario> usuarios = manager.searchAllUsuarios(apellido);
+		//Creamos un objeto Observable donde se guardarán todos los objetos usuarios.
+		ObservableList<Usuario> listaUsuarios = FXCollections.observableArrayList(usuarios);
+		
+		
+		//se envia a la celda el parametro a mostar
+		columSocio.setCellValueFactory(new PropertyValueFactory<Usuario, String>("Nsocio"));
+		columDni.setCellValueFactory(new PropertyValueFactory<Usuario, String>("dni"));
+		columNombre.setCellValueFactory(new PropertyValueFactory<Usuario, String>("nombre"));
+		columApellidos.setCellValueFactory(new PropertyValueFactory<Usuario, String>("apellidos"));
+		
+		
+		//Añadimos los Usuarios a la tabla.
+		tabla_user.setItems(listaUsuarios);
+		
+	}
+	
 
 }
