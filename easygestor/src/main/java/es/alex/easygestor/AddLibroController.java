59ca6aeda26a6dfee94982d29fe0ae2744c04bf8
AlexBCa <alexbeca92@gmail.com>
2021-04-27 18:36:07 +0200

    Añadido la función alerta y getNewLibro

--- easygestor/src/main/java/es/alex/easygestor/AddLibroController.java
+++ easygestor/src/main/java/es/alex/easygestor/AddLibroController.java
@@ -5,14 +5,17 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.Libro;
+import model.Usuario;
 
 public class AddLibroController implements Initializable {
 	
     @FXML
-    private TextField textName;
+    private TextField textTitulo;
 
     @FXML
     private TextField textAutor;
@@ -31,6 +34,8 @@ public class AddLibroController implements Initializable {
 
     @FXML
     private TextField textIsbn;
+    
+    private Libro libro;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -40,7 +45,50 @@ public class AddLibroController implements Initializable {
 
 	public void initData(Libro selectedItem) {
 		// TODO Auto-generated method stub
+		this.libro = selectedItem;
+		textTitulo.setText(libro.getTitulo());
+		textAutor.setText(libro.getAutores());
+		textEditorial.setText(libro.getEditorial());
+		textEdicion.setText(libro.getEdicion());
+		textIsbn.setText(Integer.toString(libro.getIsbn()));
 		
+		
+		
+	}
+	
+	
+	public Libro getNewLibro() {
+		Libro newLibro = null;
+		try {
+			
+			newLibro = new Libro();
+			newLibro.setTitulo(textTitulo.getText());
+			newLibro.setAutores(textAutor.getText());
+			newLibro.setEditorial(textEditorial.getText());
+			newLibro.setEdicion(textEdicion.getText());
+			newLibro.setIsbn(Integer.parseInt(textIsbn.getText()));
+
+			
+		}
+		catch(NumberFormatException e) {
+			e.printStackTrace();
+			alerta("Tipo de dato erroneo",AlertType.ERROR);
+		}
+		
+		
+		return newLibro;
+		
+	}
+	
+	/**
+	 * Lanza una alerta al usuario.
+	 * @param nombre
+	 * @param alert
+	 */
+	public void alerta(String nombre, Alert.AlertType alert) {
+		Alert alerta = new Alert(alert, nombre);
+		
+		alerta.showAndWait();
 	}
 
 }
