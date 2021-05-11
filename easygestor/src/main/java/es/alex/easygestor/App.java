b70d7dabc76b006efd133d628819de4ce9fc83ac
AlexBCa <alexbeca92@gmail.com>
2021-05-11 20:27:56 +0200

    Añadimos el icono de la aplicación al stage.

--- easygestor/src/main/java/es/alex/easygestor/App.java
+++ easygestor/src/main/java/es/alex/easygestor/App.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import com.mysql.cj.xdevapi.Client;
 
 /**
  * JavaFX App
@@ -17,13 +20,27 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 680, 400);
+    	
+        scene = new Scene(loadFXML("primary"), 750, 420); //680 400
+        //scene.getStylesheets().add(getClass().getResource("resources/es/alex/css/DarkTheme.css").toExternalForm());
+        //scene.getStylesheets().add(getClass().getResource("resources/es/alex/css/DarkTheme.css").toExternalForm());
+        
+        //scene.getStylesheets().add(getClass().getResource("/css/DarkTheme.css").toString());
+        
+        
+        // Importantisimo la ruta no puede empezar por /
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("image/ico.png")));
+        
+        
         stage.setScene(scene);
+        
         stage.setResizable(false);
+        
         stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
+    	
         scene.setRoot(loadFXML(fxml));
     }
 
